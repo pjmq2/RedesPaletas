b6fa3e6eb73a9f2ddab7646d0a00fc453b2ec532
Pablo Madrigal <pjmq2@hotmail.com>
2017-11-07 17:12:42 -0600

    Cambios iniciales

--- src/ac/cr/ucr/ci1320/Main.java
+++ src/ac/cr/ucr/ci1320/Main.java
@@ -4,12 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //Controller controller = new Controller();
-        //controller.startController();
-        // Server server = new Server(5555,"localhost", null, null);
-        //server.startServer();
-        Client client = new Client(null, null, null);
-        client.startClient2();
+
 
     }
 }
