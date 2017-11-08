9c96dfc1767f378ded36bc9dd5872611d2d27ed1
Pablo Madrigal <pjmq2@hotmail.com>
2017-11-08 03:13:00 -0600

    Tablas creadas

--- src/ac/cr/ucr/ci1320/Main.java
+++ src/ac/cr/ucr/ci1320/Main.java
@@ -30,6 +30,43 @@ public class Main {
         Nodo nodoPablo = new Nodo(tablaD,"192.168.0.121",7777);
     }
 
+    public static void julian(){
+        Map<String,TablaDirecciones> tablaD= new HashMap<>();
+
+        TablaDirecciones tabla2 = new TablaDirecciones("CRR2","12.0.0.7",1,5555);
+        TablaDirecciones tabla3 = new TablaDirecciones("CRR3","12.0.0.7",1,5555);
+        TablaDirecciones tabla4 = new TablaDirecciones("TELO1","12.0.0.7",2,5555);
+        TablaDirecciones tabla5 = new TablaDirecciones("LEOG2","12.0.20.2",3,5555);
+        TablaDirecciones tabla6 = new TablaDirecciones("Bolinchas.Jorge","12.0.20.2",2,2222);
+        TablaDirecciones tabla7 = new TablaDirecciones("P2","12.0.20.2",1,2222);
+
+        tablaD.put("165.8.2.0",tabla2);
+        tablaD.put("165.8.48.2",tabla3);
+        tablaD.put("25.0.2.5",tabla4);
+        tablaD.put("201.6.0.2",tabla5);
+        tablaD.put("140.90.0.20",tabla6);
+        tablaD.put("200.5.0.2",tabla7);
+        Nodo nodoPablo = new Nodo(tablaD,"192.168.0.121",7777);
+    }
+
+    public static void sebastian(){
+        Map<String,TablaDirecciones> tablaD= new HashMap<>();
+        TablaDirecciones tabla1 = new TablaDirecciones("Julian","12.0.0.8",0,3333);
+        TablaDirecciones tabla2 = new TablaDirecciones("CRR2","12.0.0.7",1,5555);
+        TablaDirecciones tabla3 = new TablaDirecciones("CRR3","12.0.0.7",1,5555);
+        TablaDirecciones tabla4 = new TablaDirecciones("TELO1","12.0.0.7",2,5555);
+        TablaDirecciones tabla5 = new TablaDirecciones("LEOG2","200.5.0.1",2,5555);
+        TablaDirecciones tabla6 = new TablaDirecciones("Bolinchas.Jorge","200.5.0.1",1,2222);
+        TablaDirecciones tabla7 = new TablaDirecciones("P2","200.5.0.1",0,2222);
+        tablaD.put("12.0.0.8",tabla1);
+        tablaD.put("165.8.2.0",tabla2);
+        tablaD.put("165.8.48.2",tabla3);
+        tablaD.put("25.0.2.5",tabla4);
+        tablaD.put("201.6.0.2",tabla5);
+        tablaD.put("140.90.0.20",tabla6);
+        tablaD.put("200.5.0.2",tabla7);
+        Nodo nodoPablo = new Nodo(tablaD,"192.168.0.121",7777);
+    }
 }
 
 
