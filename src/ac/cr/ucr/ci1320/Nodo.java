601f6f9717449b0d31e6342f060b917c9e3f9a4c
julianx2rl <julian.arguedastorres@gmail.com>
2017-11-14 19:07:40 -0600

    Update Nodo.java

--- src/ac/cr/ucr/ci1320/Nodo.java
+++ src/ac/cr/ucr/ci1320/Nodo.java
@@ -11,13 +11,13 @@ public class Nodo {
     public int miPuerto;
     private Analizador analisis;
 
-    public Nodo(HashMap<String, TablaDirecciones> tablaD, String miIp, int miPuerto, HashMap<String,String>tablaIP, Dispatcher dis) {
+    public Nodo(HashMap<String, TablaDirecciones> tablaD, String miIp, int miPuerto, HashMap<String,String>tablaIP) {
         this.tablaD = tablaD;
         this.miIp = miIp;
         this.miPuerto = miPuerto;
         this.tablaIP = tablaIP;
         this.analisis = new Analizador(tablaD, tablaIP, miIp);
-        this.dispon = dis;
+        this.dispon = new Dispatcher("12.0.0.7", this.tablaIP.get("12.0.0.7"), "12.0.20.2", this.tablaIP.get("12.0.20.2"), "12.0.0.8", this.tablaIP.get("12.0.0.8"), 9999, this);;
     }
 
     public void iniciar()
