433da7a01986a041a19215dbbccc5fb2cd6b2736
julianx2rl <julian.arguedastorres@gmail.com>
2017-11-14 18:12:37 -0600

    Update Dispatcher.java

--- src/ac/cr/ucr/ci1320/Dispatcher.java
+++ src/ac/cr/ucr/ci1320/Dispatcher.java
@@ -13,40 +13,60 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Dispatcher {
-    private Map<String,String> tablaIP;
+    private HashMap<String,String> tablaIP;
     String localIP;
+    int port;
 
-    public Dispatcher(String miIP) {
+    public Dispatcher(String miIP, int backport) {
         this.tablaIP = new HashMap<>();
-        tablaIP.put("12.0.0.7","000.000.0.000"); // P
-        tablaIP.put("12.0.20.2","000.000.0.000"); // S
-        tablaIP.put("12.0.0.8",miIP); // J
-        localIP = miIP;
+        this.tablaIP.put("12.0.0.7","000.000.0.000"); // P
+        this.tablaIP.put("12.0.20.2","000.000.0.000"); // S
+        this.tablaIP.put("12.0.0.8","000.000.0.000"); // J
+        this.tablaIP.put("12.0.0.8",miIP); // CON IP FALSA DE JULIÁN
+        this.localIP = miIP;
+        this.port = backport;
+    }
+
+    public HashMap<String,String> BuildIPTable(String fake1, String real1, String fake2, String real2, String fake3, String real3) {
+        HashMap<String,String> tablonIP = new HashMap<>();
+        tablonIP.put(fake1,real1); // P
+        tablonIP.put(fake2,real2); // S
+        tablonIP.put(fake3,real3); // J
+        return tablonIP;
     }
 
     public String getTablaIPString() {
-        return tablaIP.toString();
+        String returnValue = new String();
+        returnValue = returnValue + "12.0.0.7=" + tablaIP.get("12.0.0.7") + "\n";
+        returnValue = returnValue + "12.0.20.2=" + tablaIP.get("12.0.0.7") + "\n";
+        returnValue = returnValue + "12.0.0.8=" + tablaIP.get("12.0.0.7");
+        return returnValue;
     }
 
-    public Map<String,String> getTablaIP() {
+    public HashMap<String,String> getTablaIP() {
         return tablaIP;
     }
 
     public void iniciar() {
-        Thread starter = new Thread(new Dispatcher.Starter());
+        Thread starter = new Thread(new Dispatcher.Starter(port));
         starter.start();
         System.out.println("\nDispatcher esperando...");
     }
 
     public class Starter implements Runnable
     {
+        int puerto;
+
+        public Starter(int backport) {
+            puerto = backport;
+        }
+
         public void run(){
             try
             {
-                ServerSocket servidor = new ServerSocket(7777);
+                ServerSocket servidor = new ServerSocket(puerto);
                 while (true){
                     Socket cliente = servidor.accept();
                     PrintWriter writer = new PrintWriter(cliente.getOutputStream());
@@ -92,7 +112,7 @@ public class Dispatcher {
                 DataInputStream outClient;
                 outClient = new DataInputStream(sock.getInputStream());
                 String mensaje = outClient.readUTF();
-                System.out.println(mensaje);
+                System.out.println(getTablaIPString());
             }
             catch (Exception ex)
             {
