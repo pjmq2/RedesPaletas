e7d380214b8edb3f73c54eb1d02b98130eeb8b8b
a4alonso03 <alonso.garita1@gmail.com>
2017-12-08 11:12:55 -0600

    Parte de los buffers

---
+++ src/ac/cr/ucr/ci1320/Etapa3/IpPackage.java
@@ -0,0 +1,45 @@
+public class IpPackage {
+    private String realIP;
+    private String fakeIP;
+    private Integer port;
+    private String message;
+
+    public IpPackage(String inputMessage){
+        this.message = inputMessage;
+    }
+
+
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRealIP() {
+        return realIP;
+    }
+
+    public void setRealIP(String realIP) {
+        this.realIP = realIP;
+    }
+
+    public String getFakeIP() {
+        return fakeIP;
+    }
+
+    public void setFakeIP(String fakeIP) {
+        this.fakeIP = fakeIP;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+}
---
+++ src/ac/cr/ucr/ci1320/Etapa3/ProcessingThread.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class ProcessingThread implements  Runnable {
+    public DataStructures dataStructures;
+    private Random random;
+
+    public ProcessingThread(DataStructures inputStructures){
+        this.dataStructures = inputStructures;
+        random = new Random();
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Processing thread starting");
+
+        for(;;){
+            try {
+                BufferEntry bufferReference = dataStructures.getpQueue().take();  //Get next item from pQueue, wait if theres none
+                System.out.println("Processing data from " + bufferReference.getIpPackage().getMessage());
+                Thread.sleep(random.nextInt(10) * 100);   //Process it
+                dataStructures.getEmptyPositions().put(bufferReference.getArrayPosition());
+                System.out.println("Position: " + bufferReference.getArrayPosition() + " Logically erased");
+            }catch (InterruptedException e){
+                e.printStackTrace();
+
+            }
+        }
+
+    }
+
+    /**
+     * Sacar la referencia del siguiente a sacar
+     *
+     * Procesarlo
+     *
+     * Agregar la posición del mae y agregarla a emptyPositions
+     */
+}
